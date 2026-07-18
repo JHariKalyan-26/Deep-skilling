44455a255aa7d0d981f7ee6d764dfc2c43fb0c2c
Your Name <you@example.com>
2026-07-18 12:07:43 +0530

    Completed NUnit and Moq module

---
+++ NUnit and Moq/NUnitMoqHandsOn/HandsOnLib.Tests/AllHandsOnTests.cs
@@ -0,0 +1,229 @@
+﻿using HandsOnLib;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HandsOnLib.Tests;
+
+[TestFixture]
+public class CalculatorTests
+{
+    private SimpleCalculator _calculator = null!;
+
+    [SetUp]
+    public void Setup() => _calculator = new SimpleCalculator();
+
+    [TestCase(10, 20, 30)]
+    [TestCase(-5, 5, 0)]
+    public void Addition_ValidNumbers_ReturnsSum(double a, double b, double expected) =>
+        Assert.That(_calculator.Addition(a, b), Is.EqualTo(expected));
+
+    [TestCase(10, 5, 2)]
+    public void Division_ValidNumbers_ReturnsQuotient(double a, double b, double expected) =>
+        Assert.That(_calculator.Division(a, b), Is.EqualTo(expected));
+
+    [Test]
+    public void Division_ZeroDivisor_ThrowsArgumentException() =>
+        Assert.That(() => _calculator.Division(10, 0), Throws.TypeOf<ArgumentException>());
+
+    [Test]
+    public void AllClear_AfterCalculation_ResultBecomesZero()
+    {
+        _calculator.Addition(10, 5);
+        _calculator.AllClear();
+        Assert.That(_calculator.GetResult, Is.Zero);
+    }
+}
+
+[TestFixture]
+public class ConverterTests
+{
+    private Converter CreateConverter(double rate = 0.9)
+    {
+        var feed = new Mock<IDollarToEuroExchangeRateFeed>();
+        feed.Setup(x => x.GetActualUSDollarValue()).Returns(rate);
+        return new Converter(feed.Object);
+    }
+
+    [TestCase(0, 273.15)]
+    [TestCase(100, 373.15)]
+    public void CelsiusToKelvin_ValidInput_ReturnsExpected(double input, double expected) =>
+        Assert.That(CreateConverter().CelsiusToKelvin(input), Is.EqualTo(expected).Within(0.001));
+
+    [TestCase(10, 0.9, 9)]
+    [TestCase(100, 0.85, 85)]
+    public void USDToEuro_MockedRate_ReturnsExpected(double dollars, double rate, double expected) =>
+        Assert.That(CreateConverter(rate).USDToEuro(dollars), Is.EqualTo(expected).Within(0.001));
+}
+
+[TestFixture]
+public class UrlHostNameParserTests
+{
+    [TestCase("https://www.cognizant.com/in/en", "www.cognizant.com")]
+    [TestCase("http://example.com/test", "example.com")]
+    public void ParseHostName_ValidUrl_ReturnsHost(string url, string expected) =>
+        Assert.That(new UrlHostNameParser().ParseHostName(url), Is.EqualTo(expected));
+
+    [Test]
+    public void ParseHostName_InvalidProtocol_ThrowsFormatException() =>
+        Assert.That(() => new UrlHostNameParser().ParseHostName("ftp://example.com"),
+            Throws.TypeOf<FormatException>());
+}
+
+[TestFixture]
+public class AccountsManagerTests
+{
+    [TestCase("user_11", "secret@user11", "Welcome user_11!!!")]
+    [TestCase("user_22", "secret@user22", "Welcome user_22!!!")]
+    [TestCase("wrong", "wrong", "Invalid user id/password")]
+    public void ValidateUser_Credentials_ReturnExpectedMessage(
+        string user, string password, string expected) =>
+        Assert.That(new AccountsManager().ValidateUser(user, password), Is.EqualTo(expected));
+
+    [TestCase("", "password")]
+    [TestCase("user", "")]
+    public void ValidateUser_MissingCredential_ThrowsFormatException(string user, string password) =>
+        Assert.That(() => new AccountsManager().ValidateUser(user, password),
+            Throws.TypeOf<FormatException>());
+}
+
+[TestFixture]
+public class EmployeeManagerTests
+{
+    private List<Employee> Employees => new EmployeeManager().GetEmployees();
+
+    [Test]
+    public void GetEmployees_Result_HasNoNullValues() =>
+        Assert.That(Employees, Has.None.Null);
+
+    [Test]
+    public void GetEmployees_Result_ContainsEmployee100() =>
+        Assert.That(Employees.Any(x => x.EmpId == 100), Is.True);
+
+    [Test]
+    public void GetEmployees_Result_IsUnique() =>
+        Assert.That(Employees.Distinct().Count(), Is.EqualTo(Employees.Count));
+
+    [Test]
+    public void PreviousEmployees_Result_EqualsAllEmployees() =>
+        Assert.That(new EmployeeManager().GetEmployeesWhoJoinedInPreviousYears(),
+            Is.EquivalentTo(Employees));
+}
+
+[TestFixture]
+public class SeasonTellerTests
+{
+    public static IEnumerable<TestCaseData> SeasonCases()
+    {
+        yield return new TestCaseData("February", "Spring");
+        yield return new TestCaseData("April", "Summer");
+        yield return new TestCaseData("August", "Monsoon");
+        yield return new TestCaseData("October", "Autumn");
+        yield return new TestCaseData("December", "Winter");
+        yield return new TestCaseData("Unknown", "Invalid Season");
+    }
+
+    [TestCaseSource(nameof(SeasonCases))]
+    public void DisplaySeasonBy_Month_ReturnsExpectedSeason(string month, string expected) =>
+        Assert.That(new SeasonTeller().DisplaySeasonBy(month), Is.EqualTo(expected));
+}
+
+[TestFixture]
+public class LeapYearCalculatorTests
+{
+    [TestCase(2000, 1)]
+    [TestCase(2024, 1)]
+    [TestCase(1900, 0)]
+    [TestCase(2023, 0)]
+    [TestCase(1752, -1)]
+    [TestCase(10000, -1)]
+    public void IsLeapYear_Year_ReturnsExpected(int year, int expected) =>
+        Assert.That(new LeapYearCalculator().IsLeapYear(year), Is.EqualTo(expected));
+}
+
+[TestFixture]
+public class UserTests
+{
+    [Test]
+    public void CreateUser_NullPan_ThrowsNullReferenceException()
+    {
+        var user = new User { PANCardNo = null! };
+        Assert.That(() => user.CreateUser(user), Throws.TypeOf<NullReferenceException>());
+    }
+
+    [Test]
+    public void CreateUser_InvalidLength_ThrowsFormatException()
+    {
+        var user = new User { PANCardNo = "ABCDE123" };
+        Assert.That(() => user.CreateUser(user), Throws.TypeOf<FormatException>());
+    }
+
+    [Test]
+    public void CreateUser_ValidPan_DoesNotThrow()
+    {
+        var user = new User { PANCardNo = "ABCDE1234F" };
+        Assert.That(() => user.CreateUser(user), Throws.Nothing);
+    }
+}
+
+[TestFixture]
+public class CustomerCommTests
+{
+    [Test]
+    public void SendMailToCustomer_MockedSender_ReturnsTrue()
+    {
+        var sender = new Mock<IMailSender>();
+        sender.Setup(x => x.SendMail(It.IsAny<string>(), It.IsAny<string>())).Returns(true);
+
+        var result = new CustomerComm(sender.Object).SendMailToCustomer();
+
+        Assert.That(result, Is.True);
+    }
+}
+
+[TestFixture]
+public class DirectoryExplorerTests
+{
+    [Test]
+    public void GetFiles_MockedDirectory_ReturnsExpectedFiles()
+    {
+        const string file1 = "file.txt";
+        const string file2 = "file2.txt";
+
+        var explorer = new Mock<IDirectoryExplorer>();
+        explorer.Setup(x => x.GetFiles(It.IsAny<string>()))
+            .Returns(new List<string> { file1, file2 });
+
+        var files = explorer.Object.GetFiles("dummy-path");
+
+        Assert.That(files, Is.EquivalentTo(new[] { file1, file2 }));
+    }
+}
+
+[TestFixture]
+public class PlayerTests
+{
+    [Test]
+    public void RegisterNewPlayer_NewName_AddsPlayer()
+    {
+        var mapper = new Mock<IPlayerMapper>();
+        mapper.Setup(x => x.IsPlayerNameExistsInDb("Hari")).Returns(false);
+
+        var player = Player.RegisterNewPlayer("Hari", mapper.Object);
+
+        Assert.That(player.Name, Is.EqualTo("Hari"));
+        mapper.Verify(x => x.AddNewPlayerIntoDb("Hari"), Times.Once);
+    }
+
+    [Test]
+    public void RegisterNewPlayer_ExistingName_ThrowsException()
+    {
+        var mapper = new Mock<IPlayerMapper>();
+        mapper.Setup(x => x.IsPlayerNameExistsInDb("Hari")).Returns(true);
+
+        Assert.That(() => Player.RegisterNewPlayer("Hari", mapper.Object),
+            Throws.TypeOf<InvalidOperationException>());
+    }
+}
---
+++ NUnit and Moq/NUnitMoqHandsOn/HandsOnLib/HandsOnModels.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace HandsOnLib;
+
+// HANDSON 1 - Calculator
+public class SimpleCalculator
+{
+    private double _result;
+
+    public double Addition(double a, double b) => _result = a + b;
+    public double Subtraction(double a, double b) => _result = a - b;
+    public double Multiplication(double a, double b) => _result = a * b;
+
+    public double Division(double a, double b)
+    {
+        if (b == 0) throw new ArgumentException("Second parameter cannot be zero.");
+        return _result = a / b;
+    }
+
+    public void AllClear() => _result = 0;
+    public double GetResult => _result;
+}
+
+// HANDSON 2 - Converter
+public interface IDollarToEuroExchangeRateFeed
+{
+    double GetActualUSDollarValue();
+}
+
+public class Converter
+{
+    private readonly IDollarToEuroExchangeRateFeed _feed;
+
+    public Converter(IDollarToEuroExchangeRateFeed feed) => _feed = feed;
+
+    public double CelsiusToKelvin(double value) => value + 273.15;
+    public double KilogramToPound(double value) => value * 2.205;
+    public double KilometerToMile(double value) => value / 1.609;
+    public double LiterToGallon(double value) => value / 3.785;
+    public double USDToEuro(double value) => value * _feed.GetActualUSDollarValue();
+}
+
+// HANDSON 3 - URL parser
+public class UrlHostNameParser
+{
+    public string ParseHostName(string url)
+    {
+        if (!Uri.TryCreate(url, UriKind.Absolute, out var uri) ||
+            (uri.Scheme != Uri.UriSchemeHttp && uri.Scheme != Uri.UriSchemeHttps))
+        {
+            throw new FormatException("URL is not in correct format.");
+        }
+
+        return uri.Host;
+    }
+}
+
+// HANDSON 4 - Accounts manager
+public class AccountsManager
+{
+    public string ValidateUser(string userId, string password)
+    {
+        if (string.IsNullOrEmpty(userId) || string.IsNullOrEmpty(password))
+            throw new FormatException("Both user id and password are mandatory.");
+
+        return (userId, password) switch
+        {
+            ("user_11", "secret@user11") => "Welcome user_11!!!",
+            ("user_22", "secret@user22") => "Welcome user_22!!!",
+            _ => "Invalid user id/password"
+        };
+    }
+}
+
+// HANDSON 5 - Collections
+public class Employee : IEquatable<Employee>
+{
+    public int EmpId { get; set; }
+    public string EmpName { get; set; } = string.Empty;
+    public double Salary { get; set; }
+    public DateTime DOJ { get; set; }
+
+    public bool Equals(Employee? other) => other is not null && EmpId == other.EmpId;
+    public override bool Equals(object? obj) => Equals(obj as Employee);
+    public override int GetHashCode() => EmpId.GetHashCode();
+}
+
+public class EmployeeManager
+{
+    private readonly List<Employee> _employees =
+    [
+        new() { EmpId = 100, EmpName = "John", DOJ = DateTime.Today.AddYears(-5), Salary = 30000 },
+        new() { EmpId = 101, EmpName = "Mary", DOJ = DateTime.Today.AddYears(-2), Salary = 10000 },
+        new() { EmpId = 102, EmpName = "Steve", DOJ = DateTime.Today.AddYears(-2), Salary = 10000 },
+        new() { EmpId = 103, EmpName = "Allen", DOJ = DateTime.Today.AddYears(-7), Salary = 50000 }
+    ];
+
+    public List<Employee> GetEmployees() => _employees;
+    public List<Employee> GetEmployeesWhoJoinedInPreviousYears() =>
+        _employees.Where(x => x.DOJ < DateTime.Today).ToList();
+}
+
+// HANDSON 6 - Seasons
+public class SeasonTeller
+{
+    public string DisplaySeasonBy(string monthName) =>
+        monthName.ToLowerInvariant() switch
+        {
+            "february" or "march" => "Spring",
+            "april" or "may" or "june" => "Summer",
+            "july" or "august" or "september" => "Monsoon",
+            "october" or "november" => "Autumn",
+            "december" or "january" => "Winter",
+            _ => "Invalid Season"
+        };
+}
+
+// HANDSON 7 - Leap year
+public class LeapYearCalculator
+{
+    public int IsLeapYear(int year)
+    {
+        if (year < 1753 || year > 9999) return -1;
+        return DateTime.IsLeapYear(year) ? 1 : 0;
+    }
+}
+
+// HANDSON 8 - User manager
+public class User
+{
+    public string PANCardNo { get; set; } = string.Empty;
+
+    public string ValidatePANCardNumber(string panCard)
+    {
+        if (string.IsNullOrEmpty(panCard))
+            throw new NullReferenceException("Invalid PAN card number.");
+
+        if (panCard.Length != 10)
+            throw new FormatException("PAN card number should contain 10 characters.");
+
+        return "Valid";
+    }
+
+    public void CreateUser(User user) => ValidatePANCardNumber(user.PANCardNo);
+}
+
+// MOQ - Mail sender
+public interface IMailSender
+{
+    bool SendMail(string toAddress, string message);
+}
+
+public class CustomerComm
+{
+    private readonly IMailSender _mailSender;
+
+    public CustomerComm(IMailSender mailSender) => _mailSender = mailSender;
+
+    public bool SendMailToCustomer() =>
+        _mailSender.SendMail("cust123@abc.com", "Some Message");
+}
+
+// MOQ - Files
+public interface IDirectoryExplorer
+{
+    ICollection<string> GetFiles(string path);
+}
+
+public class DirectoryExplorer : IDirectoryExplorer
+{
+    public ICollection<string> GetFiles(string path) => Directory.GetFiles(path);
+}
+
+// MOQ - Player database
+public interface IPlayerMapper
+{
+    bool IsPlayerNameExistsInDb(string name);
+    void AddNewPlayerIntoDb(string name);
+}
+
+public class Player
+{
+    public string Name { get; }
+
+    private Player(string name) => Name = name;
+
+    public static Player RegisterNewPlayer(string name, IPlayerMapper playerMapper)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            throw new ArgumentException("Player name cannot be empty.");
+
+        if (playerMapper.IsPlayerNameExistsInDb(name))
+            throw new InvalidOperationException("Player already exists.");
+
+        playerMapper.AddNewPlayerIntoDb(name);
+        return new Player(name);
+    }
+}
